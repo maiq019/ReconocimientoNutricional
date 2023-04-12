6ab1e45974c7530a17f36726c088065f2ee69c38
Miguel Díaz <migueldh1905@gmail.com>
2023-04-12 13:37:14 +0200

    -Screenshot on camera scene

--- Assets/ITCL/VisionNutricional/Runtime/Camera/ScreenShot.cs
+++ Assets/ITCL/VisionNutricional/Runtime/Camera/ScreenShot.cs
@@ -26,6 +26,7 @@ namespace ITCL.VisionNutricional.Runtime.Camera
             texture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
             texture.Apply();
 
+            //Save the screenshot
             string screenshotName = "Screenshot_Nutrision_" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png";
 
             NativeGallery.SaveImageToGallery(texture, "Nutrision", screenshotName);
